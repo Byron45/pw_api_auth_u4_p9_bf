48e065e3a0ecfc95777b2082f9888d46a6034f81
Byron45 <brfloresa@uce.edu.ec>
2026-02-04 14:38:37 -0500

    Tarea 6

--- src/main/java/uce/edu/web/api/auth/interfaces/AuthResource.java
+++ src/main/java/uce/edu/web/api/auth/interfaces/AuthResource.java
@@ -4,33 +4,57 @@ import java.time.Instant;
 import java.util.Set;
 
 import io.smallrye.jwt.build.Jwt;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import uce.edu.web.api.auth.domain.Usuario;
+import uce.edu.web.api.auth.infraestructure.UsuarioRepository;
 
+@Path("/auth")
 public class AuthResource {
+
+    @Inject
+    private UsuarioRepository usuarioRepository;
 
     @GET
     @Path("/token")
+    @Produces(MediaType.APPLICATION_JSON)
     public TokenResponse token(
             @QueryParam("user") String user,
             @QueryParam("password") String password) {
 
-        //Donde se compara el password y usuario contra la base
-        String issuer = "matricula-auth";
-        long ttl = 3600;
+        //Buscar en la base de datos el usuario
+        Usuario usuario = usuarioRepository.findByUsername(user);
 
-        Instant now = Instant.now();
-        Instant exp = now.plusSeconds(ttl);
+        // Validar el usuario y password
+        boolean ok = usuario != null && usuario.getPassword().equals(password);
 
-        String jwt = Jwt.issuer(issuer)
-                .subject(user)
-                .groups(Set.of(role)) // roles: user / admin
-                .issuedAt(now)
-                .expiresAt(exp)
-                .sign();
+        if (ok) {
 
-        return new TokenResponse(jwt, exp.getEpochSecond(), role);
+            //Obtener el rol del usuario desde la base de datos
+            String role = usuario.getRole();
+
+            //Donde se compara el password y usuario contra la base
+            String issuer = "matricula-auth";
+            long ttl = 3600;
+
+            Instant now = Instant.now();
+            Instant exp = now.plusSeconds(ttl);
+
+            String jwt = Jwt.issuer(issuer)
+                    .subject(user)
+                    .groups(Set.of(role)) // roles: user / admin
+                    .issuedAt(now)
+                    .expiresAt(exp)
+                    .sign();
+
+            return new TokenResponse(jwt, exp.getEpochSecond(), role);
+        } else {
+            return null;
+        }
     }
 
     public static class TokenResponse {
